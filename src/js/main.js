f13d5753979caa4920f2d338bb2ebc9ad4fc52b5
AbdeLilah-Dev <contactme.abdelilah@gmail.com>
2025-05-25 18:53:14 +0100

    UPload the Updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '/src/css/style.css'
-
+import '/src/css/style.css';
 let offcanvas;
 let showOffcanvasBtn;
 
@@ -27,12 +26,30 @@ document.addEventListener("DOMContentLoaded", () => {
             tabActive.classList.add("active");
             if (window.innerWidth < 992) {
                 offcanvas.classList.remove("show")
+                console.log("is small screen")
             } else offcanvas.classList.add("show");
             obs.disconnect();
         }
 
     });
     observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    })
+    const obsForSettingBar = new MutationObserver((mutations, obs) => {
+        const settingBar = document.querySelector(".setting-bar");
+        if (settingBar) {
+            settingBar.addEventListener("click", event => {
+                if (event.target.classList.contains("settingBar-open-btn") || event.target.parentElement.classList.contains("settingBar-open-btn")) {
+                    settingBar.lastElementChild.classList.add("show");
+                } else if (event.target.classList.contains("settingBar-close-btn") || event.target.parentElement.classList.contains("settingBar-close-btn")) {
+                    settingBar.lastElementChild.classList.remove("show");
+                }
+            })
+            obs.disconnect();
+        }
+    });
+    obsForSettingBar.observe(document.body, {
         childList: true,
         subtree: true
     })
